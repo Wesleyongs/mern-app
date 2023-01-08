f40a83bb72cb3877a882842b0bb2bab9d5c862b0
Wesley Ong <72511481+Wesleyongs@users.noreply.github.com>
2023-01-09 01:31:09 +0800

    Add temp user for testing frontend without login

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
   const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  // const user = useSelector((state) => state.user);
+  const user = useSelector((state) => state.user);
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
 
   const theme = useTheme();
@@ -39,8 +39,7 @@ const Navbar = () => {
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
-  // const fullName = `${user.firstName} ${user.lastName}`;
-  const fullName = "Wesley Ong"
+  const fullName = user ? `${user.firstName} ${user.lastName}` : "Wesley Ong" ;
 
   return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
