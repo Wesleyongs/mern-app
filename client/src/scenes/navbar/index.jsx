f4acd0f502b12a212c78751b14bd4e80ce967c77
Wesley Ong <72511481+Wesleyongs@users.noreply.github.com>
2023-01-09 00:18:21 +0800

    Add Navbar desktop view

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -39,6 +39,101 @@ const Navbar = () => {
   const primaryLight = theme.palette.primary.light;
   const alt = theme.palette.background.alt;
 
-  return <div>Navbar</div>;
+  const fullName = `${user.firstName} ${user.lastName}`;
+
+  return (
+    <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+      <FlexBetween gap="1.75rem">
+        <Typography
+          fontWeight="bold"
+          fontSize="clamp(1rem, 2rem, 2.25rem)" // CSS function for min, desired and max value, in the case for the font
+          color="primary"
+          onClick={() => navigate("/home")} // route to home
+          sx={{
+            "&:hover": {
+              color: "primaryLight",
+              cursor: "pointer",
+            },
+          }}
+        ></Typography>
+        {isNonMobileScreens && (
+          <FlexBetween
+            backgroundColor={neutralLight}
+            borderRadius="9px"
+            gap="3rem"
+            padding="0.1rem 1.5rem"
+          >
+            <InputBase placeholder="Search..." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        )}
+      </FlexBetween>
+
+      {/* DESKTOP NAV */}
+      {isNonMobileScreens ? (
+        <FlexBetween gap="2rem">
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.color.mode === "dark" ? (
+              <DarkMode sx={{ fonSize: "25px" }} />
+            ) : (
+              <LightMode sx={{ fonSize: "25px" }} />
+            )}
+          </IconButton>
+          <Message sx={{ fontSize: "25px" }} />
+          <Notification sx={{ fontSize: "25px" }} />
+          <Help sx={{ fontSize: "25px" }} />
+          <FormControl variant="standard" value={fullName}>
+            <Select
+              value={fullName}
+              sx={{
+                backgroundColor: neutralLight,
+                width: "150px",
+                borderRadius: "0.25rem",
+                p: "0.25rem 1rem",
+                "& .MuiSvgIcon-root": { pr: "0.25rem", width: "3rem" },
+                "& .MuiSelect-select:focus": {
+                  backgroundColor: neutralLight,
+                },
+              }}
+              input={<InputBase />}
+            >
+              <MenuItem value={fullName}>
+                <Typography>{fullName}</Typography>
+              </MenuItem>
+              <MenuItem onClick={() => dispatch(setLogout())}>Log Out</MenuItem>
+            </Select>
+          </FormControl>
+        </FlexBetween>
+      ) : (
+        <IconButton>
+          onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
+          <Menu />
+        </IconButton>
+      )}
+      {/* MOBILE NAV */}
+      {!isNonMobileScreens && isMobileMenuToggled && (
+        <Box
+          position="fixed"
+          right="0"
+          bottom="0"
+          height="100%"
+          zIndex="10"
+          maxWidth="500px"
+          minWidth="300px"
+          backgroundColor={background}
+        >
+          {/* CLOSE ICON */}
+          <Box display="flex" justifyContent="flex-end" p="1rem">
+            <IconButton onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}>
+              <Close />
+            </IconButton>
+          </Box>
+          {/* MENU ITEMS */}
+        </Box>
+      )}
+    </FlexBetween>
+  );
 };
 export default Navbar;
