0c7dc1eec32963c3d654d2a0bf9f4c6aa87f6774
Wesley Ong <72511481+Wesleyongs@users.noreply.github.com>
2023-01-10 16:38:49 +0800

    Add config file to easily change URL variable

---
+++ client/src/config.js
@@ -0,0 +1,14 @@
+const config = {
+  app: {
+    port: 3000,
+    name: "myapp",
+    url: "http://localhost:3001/",
+  },
+  db: {
+    host: "localhost",
+    port: 27017,
+    name: "db",
+  },
+};
+
+export default config;
