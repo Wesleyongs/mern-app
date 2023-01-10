a2121ab48fa3d0f5aeb1988cc41aa4eccda74ec7
Wesley Ong <72511481+Wesleyongs@users.noreply.github.com>
2023-01-10 14:44:25 +0800

    Add login and register page

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,7 +1,34 @@
-import { Box } from "@mui/material";
-import Navbar from "scenes/navbar";
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
 
 const HomePage = () => {
-    return <Box><Navbar/></Box>;
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          Sociopedia
+        </Typography>
+      </Box>
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to Sociopedia
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
 };
 export default HomePage;
