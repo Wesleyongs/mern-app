56f2a20e2bd276c2feabf37f8203b93108c1b2c1
Wesley Ong <72511481+Wesleyongs@users.noreply.github.com>
2023-01-16 18:40:23 +0800

    Use config url instead of static localhost url

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -9,7 +9,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`http://localhost:3001/assets/${image}`}
+        src={`${config.app.url}${image}`}
       />
     </Box>
   );
--- client/src/scenes/widgets/PostWidgets.jsx
+++ client/src/scenes/widgets/PostWidgets.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setPosts } from "state";
 import PostWidget from "./PostWidget";
+import config from "config";
 
 const PostsWidget = ({ userId, isProfile = false }) => {
   const dispatch = useDispatch();
@@ -9,11 +10,32 @@ const PostsWidget = ({ userId, isProfile = false }) => {
   const token = useSelector((state) => state.token);
 
   const getPosts = async () => {
-    const response = await fetch("http://localhost:3001/posts", {
+    const response = await fetch(`${config}posts`, {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
     const data = await response.json();
     dispatch(setPosts({ posts: data }));
   };
+
+  const getUserPosts = async () => {
+    const response = await fetch(`${config}posts/${userId}/posts`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    dispatch(setPosts({ posts: data }));
+  };
+
+  useEffect(() => {
+    if (isProfile) {
+      getUserPosts();
+    } else {
+      getPosts();
+    }
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 };
+
+/* RESUME at 4:32:05 */
+
+export default PostsWidget;
