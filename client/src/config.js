7f150bc3d5ded5b06bbcb1fd4213055343d8b5d5
Wesley Ong <72511481+Wesleyongs@users.noreply.github.com>
2023-08-16 11:45:40 +0800

    Change server URL to vercel

--- client/src/config.js
+++ client/src/config.js
@@ -2,8 +2,9 @@ const config = {
   app: {
     port: 3001,
     name: "backend",
-    url: "http://localhost:3001/",
+    // url: "http://localhost:3001/",
     // url: "https://tfcivwc0ol.execute-api.ap-southeast-1.amazonaws.com/dev/",
+    url: "https://mern-app-backend-dusky.vercel.app/"
   },
   db: {
     host: "localhost",
